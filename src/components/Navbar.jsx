2dbb377f8b40a59c74c3a52841a57f47bef93a90
abir-01 <abir.rakshit@gmail.com>
2023-09-23 10:12:38 +0530

    changed favicon, title and few css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import bird from '../assets/bird.png'
 const Navbar = () => {
   return (
       
           <nav className="navbar navbar-expand-lg bg-body-tertiary m-0 p-0">
           <div className="container-fluid d-flex bg-black ">
-              <Link className="navbar-brand mx-auto text-light" to="/"><h1 className='p-4'>Chiming Birds</h1></Link>
+              <Link className="navbar-brand mx-auto text-light" to="/"><h1 className='px-4 pt-5 pb-3 position-relative'>
+                  <img src={bird} alt="" className='position-absolute bird' />
+                  <p className='title'>Chiming Birds</p>
+              </h1></Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
               </button>
